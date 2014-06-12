e56173655806dc374b92922a8081cbb2e53cb5c6
liaojinxing <jinxingliao@gmail.com>
2014-06-12 15:10:25 +0800

    detail done

--- Voice2Note/Source/Controller/NoteDetailController.m
+++ Voice2Note/Source/Controller/NoteDetailController.m
@@ -19,10 +19,7 @@
 static const CGFloat kViewOriginY = 70;
 static const CGFloat kTextFieldHeight = 30;
 static const CGFloat kToolbarHeight = 44;
-
-static const CGFloat kVoiceButtonWidth = 160;
-static const CGFloat kButtonHeight = 44;
-
+static const CGFloat kVoiceButtonWidth = 100;
 
 @interface NoteDetailController () <IFlyRecognizerViewDelegate, UIActionSheetDelegate, MFMailComposeViewControllerDelegate>
 {
@@ -30,10 +27,7 @@ static const CGFloat kButtonHeight = 44;
   UITextField *_titleTextField;
   UITextView *_contentTextView;
   
-  UIButton *_emailButton;
-  UIButton *_weixinButton;
   UIButton *_voiceButton;
-  
   IFlyRecognizerView *_iflyRecognizerView;
 }
 @end
@@ -77,11 +71,16 @@ static const CGFloat kButtonHeight = 44;
 
 - (void)setupNavigationBar
 {
-  UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"ActionSheetSave", @"")
+  UIBarButtonItem *saveItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"ActionSheetSave", @"")
                                                                 style:UIBarButtonItemStylePlain
                                                                target:self
                                                                action:@selector(save)];
-  self.navigationItem.rightBarButtonItem = rightItem;
+  
+  UIBarButtonItem *moreItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"ic_more_white"]
+                                                               style:UIBarButtonItemStylePlain
+                                                              target:self
+                                                              action:@selector(moreActionButtonPressed)];
+  self.navigationItem.rightBarButtonItems = [NSArray arrayWithObjects:moreItem, saveItem, nil];
 }
 
 - (void)setupSpeechRecognizer
@@ -106,14 +105,14 @@ static const CGFloat kButtonHeight = 44;
                                 initWithBarButtonSystemItem:UIBarButtonSystemItemDone
                                 target:self
                                 action:@selector(hideKeyboard)];
-  UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, kToolbarHeight)];
+  UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 60, kToolbarHeight)];
   toolbar.items = [NSArray arrayWithObject:barButton];
   
   _titleTextField = [[UITextField alloc] initWithFrame:frame];
   if (_note) {
     _titleTextField.text = _note.title;
   } else {
-    _titleTextField.placeholder = @"标题";
+    _titleTextField.placeholder = NSLocalizedString(@"InputViewTitle", @"");
   }
   _titleTextField.inputAccessoryView = toolbar;
   [self.view addSubview:_titleTextField];
@@ -126,7 +125,7 @@ static const CGFloat kButtonHeight = 44;
   frame = CGRectMake(kHorizontalMargin,
                      textY,
                      self.view.frame.size.width - kHorizontalMargin * 2,
-                     self.view.frame.size.height - textY - kButtonHeight * 2 - kVerticalMargin * 2);
+                     self.view.frame.size.height - textY - kVoiceButtonWidth - kVerticalMargin * 2);
   _contentTextView = [[UITextView alloc] initWithFrame:frame];
   _contentTextView.font = [UIFont systemFontOfSize:15];
   _contentTextView.autocorrectionType = UITextAutocorrectionTypeNo;
@@ -135,33 +134,17 @@ static const CGFloat kButtonHeight = 44;
   if (_note) {
     _contentTextView.text = _note.content;
   } else {
-    _contentTextView.text = @"正文";
+    _contentTextView.text = NSLocalizedString(@"InputViewText", @"");
   }
   _contentTextView.inputAccessoryView = toolbar;
   [self.view addSubview:_contentTextView];
   
-  
-  _emailButton = [UIButton buttonWithType:UIButtonTypeCustom];
-  [_emailButton setFrame:CGRectMake(kVoiceButtonWidth, self.view.frame.size.height - kButtonHeight * 2,
-                                    self.view.frame.size.width - kVoiceButtonWidth, kButtonHeight)];
-  [_emailButton setTitle:NSLocalizedString(@"ActionSheetMail", @"") forState:UIControlStateNormal];
-  [_emailButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-  [_emailButton setBackgroundColor:[UIColor orangeColor]];
-  [_emailButton addTarget:self action:@selector(sendEmail) forControlEvents:UIControlEventTouchUpInside];
-  [self.view addSubview:_emailButton];
-  
-  _weixinButton = [UIButton buttonWithType:UIButtonTypeCustom];
-  [_weixinButton setFrame:CGRectMake(kVoiceButtonWidth, self.view.frame.size.height - kButtonHeight, self.view.frame.size.width - kVoiceButtonWidth, kButtonHeight)];
-  [_weixinButton setTitle:NSLocalizedString(@"ActionSheetWeixin", @"") forState:UIControlStateNormal];
-  [_weixinButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-  [_weixinButton setBackgroundColor:[UIColor greenColor]];
-  [_weixinButton addTarget:self action:@selector(shareToWeixin) forControlEvents:UIControlEventTouchUpInside];
-  [self.view addSubview:_weixinButton];
-  
   _voiceButton = [UIButton buttonWithType:UIButtonTypeCustom];
-  [_voiceButton setFrame:CGRectMake(0, self.view.frame.size.height - kButtonHeight * 2, kVoiceButtonWidth, kButtonHeight * 2)];
+  [_voiceButton setFrame:CGRectMake((self.view.frame.size.width - kVoiceButtonWidth) / 2, self.view.frame.size.height - kVoiceButtonWidth - kVerticalMargin, kVoiceButtonWidth, kVoiceButtonWidth)];
   [_voiceButton setTitle:NSLocalizedString(@"VoiceInput", @"") forState:UIControlStateNormal];
   [_voiceButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+  _voiceButton.layer.cornerRadius = kVoiceButtonWidth / 2;
+  _voiceButton.layer.masksToBounds = YES;
   [_voiceButton setBackgroundColor:[UIColor redColor]];
   [_voiceButton addTarget:self action:@selector(startListenning) forControlEvents:UIControlEventTouchUpInside];
   [self.view addSubview:_voiceButton];
@@ -224,7 +207,7 @@ static const CGFloat kButtonHeight = 44;
                    animations:^
    {
      CGRect frame = _contentTextView.frame;
-     frame.size.height = self.view.frame.size.height - kViewOriginY - kTextFieldHeight - kButtonHeight * 2 - kVerticalMargin * 3;
+     frame.size.height = self.view.frame.size.height - kViewOriginY - kTextFieldHeight - kVoiceButtonWidth - kVerticalMargin * 3;
      _contentTextView.frame = frame;
    }               completion:NULL];
 }
@@ -259,6 +242,31 @@ static const CGFloat kButtonHeight = 44;
     [SVProgressHUD showSuccessWithStatus:NSLocalizedString(@"SaveSuccess", @"")];
   } else {
     [SVProgressHUD showSuccessWithStatus:NSLocalizedString(@"SaveFail", @"")];
+  }
+}
+
+#pragma mark - More Action
+
+- (void)moreActionButtonPressed
+{
+  [self hideKeyboard];
+  UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:nil
+                                                           delegate:self
+                                                  cancelButtonTitle:NSLocalizedString(@"ActionSheetCancel", @"")
+                                             destructiveButtonTitle:nil
+                                                  otherButtonTitles:NSLocalizedString(@"ActionSheetMail", @""),
+                                NSLocalizedString(@"ActionSheetWeixin", @""), nil];
+  [actionSheet showInView:self.view];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+  if (buttonIndex == 0) {
+    if ([MFMailComposeViewController canSendMail]) {
+      [self sendEmail];
+    }
+  } else if (buttonIndex == 1) {
+    [self shareToWeixin];
   }
 }
 
