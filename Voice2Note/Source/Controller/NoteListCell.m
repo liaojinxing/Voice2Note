8cea5f7f79600e7024e3a47ad0484a9b7cdf725e
liaojinxing <jinxingliao@gmail.com>
2015-02-09 18:45:59 +0800

    delete widget

--- Voice2Note/Source/Controller/NoteListCell.m
+++ Voice2Note/Source/Controller/NoteListCell.m
@@ -16,7 +16,7 @@ static const CGFloat kCellPadding = 15;
 static const CGFloat kVerticalPadding = 0;
 static const CGFloat kLabelHeight = 15;
 
-static const CGFloat kMaxTitleHeight = 100;
+static const CGFloat kMaxTitleHeight = 180;
 
 @interface NoteListCell ()
 {
